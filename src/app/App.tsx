b84bad4f192e805f3bb409583768e3b9286852d4
Emili Srdoc <emili@srdoc.si>
2025-04-01 18:39:47 +0200

    chore: remove sample content

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold">Task Manager</h1>
+    </div>
+  );
+};
+
+export default App;
