1a1c2b026cd644507ed4ac52f6be1eeb819e9f60
Emili Srdoc <emili@srdoc.si>
2025-04-02 20:36:15 +0200

    feat: features folder structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,7 @@
-import { FC } from "react";
-
-const App: FC = () => {
+export default function App() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold text-blue-500">Task Manager</h1>
     </div>
   );
-};
-
-export default App;
+}
